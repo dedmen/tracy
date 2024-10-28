ac58135bb7d6db2dd285482d5c75702548af861f
Dedmen Miller <dedmen@dedmen.de>
2024-10-28 14:47:06 +0100

    Compile without python

--- profiler/src/main.cpp
+++ profiler/src/main.cpp
@@ -68,7 +68,7 @@
 #include "ResolvService.hpp"
 #include "RunQueue.hpp"
 
-#include "GitRef.hpp"
+//#include "GitRef.hpp"
 
 
 struct ClientData
@@ -716,7 +716,7 @@ static void DrawContents()
             ImGui::PopFont();
             ImGui::PushFont( s_smallFont );
             ImGui::PushStyleColor( ImGuiCol_Text, GImGui->Style.Colors[ImGuiCol_TextDisabled] );
-            tracy::TextCentered( tracy::GitRef );
+            //tracy::TextCentered( tracy::GitRef );
             ImGui::PopStyleColor();
             ImGui::PopFont();
             if( ImGui::IsItemHovered() )
@@ -726,7 +726,7 @@ static void DrawContents()
                 ImGui::EndTooltip();
                 if( ImGui::IsItemClicked() )
                 {
-                    ImGui::SetClipboardText( tracy::GitRef );
+                    //ImGui::SetClipboardText( tracy::GitRef );
                 }
             }
 #ifndef NDEBUG
